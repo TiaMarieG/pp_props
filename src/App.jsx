7054ae289fa56ea96f720884888bc4b9c4d9a827
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-10 11:42:19 -0700

    Edited the description of one of the hobbies

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const hobbyList = [
     {title: 'Web Design', description: 'Creating guide websites for games'},
     {title: 'Gaming', description: 'Playing various games that I enjoy'},
-    {title: 'Cross-stitching', description: 'Threads in shape of an x to make an overall pattern'}
+    {title: 'Cross-stitching', description: 'Threads in shape of an \'X\' to make an overall pattern'}
   ]
 
   return (
