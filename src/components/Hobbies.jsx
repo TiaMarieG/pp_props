730ab869803178e55adb30b25779517e3364c124
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-10 11:39:28 -0700

    Added a comment to Hobbies.jsx

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,6 +1,7 @@
 import Hobby from "./Hobby.jsx"
 
-function Hobbies( { hobbies }) {
+// Passing hobbies in from App.jsx to this function as a parameter and using it with a map
+function Hobbies({ hobbies }) {
    return(
       <div>
          {hobbies.map((hobby, index) => <Hobby key={`${index}${hobby.title}`} title={hobby.title} description={hobby.description} />)}
